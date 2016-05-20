c6c07008ae078517f6cecce107c307ed884359d3
dusterherz <dusterherz@gmail.com>
2016-05-20 04:31:49 -0400

    Create the packet start with user, opponent and monster.

---
+++ server/src/net/msg/combat/data/start/opponent.rs
@@ -0,0 +1,31 @@
+use net::msg::{MessagePart};
+use net::msg::error::Result;
+use net::msg::utils::fields;
+use net::msg::utils::rmp::ValueExt;
+use rmp::Value;
+use super::super::user::User;
+use super::super::monster::Monster;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Opponent {
+    pub monster: Monster,
+    pub monsters_count: u8,
+    pub user: User,
+}
+
+impl MessagePart for Opponent {
+    fn decode(data: &Value) -> Result<Opponent> {
+        Ok(Opponent {
+            monster: try!(Monster::decode(data)),
+            monsters_count: try!(fields::get(data, "monsters_count")),
+            user: try!(User::decode(data)),
+        })
+    }
+
+    fn encode(&self, data: &mut Value) {
+        data.set("type", "combat:start:Opponent");
+        self.monster.encode(data);
+        self.user.encode(data);
+        data.set("monsters_count", &self.monsters_count);
+    }
+}
