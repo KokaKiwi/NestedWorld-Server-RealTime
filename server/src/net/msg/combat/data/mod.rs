15f94632d14ec18972f9a4621053db31ac19f882
dusterherz <dusterherz@gmail.com>
2016-05-20 06:05:02 -0400

    Added packet attack_received with combat:attack-received:monster

--- server/src/net/msg/combat/data/mod.rs
+++ server/src/net/msg/combat/data/mod.rs
@@ -2,3 +2,4 @@ pub mod start;
 
 pub mod user;
 pub mod monster;
+pub mod attack_received;
--- server/src/net/msg/combat/mod.rs
+++ server/src/net/msg/combat/mod.rs
@@ -1,5 +1,6 @@
 pub mod data;
 
 pub mod available;
+pub mod attack_received;
 pub mod start;
 pub mod send_attack;
