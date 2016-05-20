466381f49b62b110e05fa9272d317ebe85f716ec
dusterherz <dusterherz@gmail.com>
2016-05-20 04:49:51 -0400

    Add the packet attack to the combat

---
+++ server/src/net/msg/combat/attack.rs
@@ -0,0 +1,34 @@
+use net::msg::{MessagePart, MessageHeader};
+use net::msg::error::{Result};
+use net::msg::states::auth::Authenticated;
+use net::msg::utils::fields;
+use net::msg::utils::rmp::ValueExt;
+use rmp::Value;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct SendAttack {
+    pub header: MessageHeader,
+    pub auth: Authenticated,
+    pub target: u32,
+    pub attack: u32,
+}
+
+impl MessagePart for SendAttack {
+    fn decode(data: &Value) -> Result<SendAttack> {
+        Ok(SendAttack {
+            header: try!(MessageHeader::decode(data)),
+            auth: try!(Authenticated::decode(data)),
+            target: try!(fields::get(data, "target")),
+            attack: try!(fields::get(data, "attack")),
+        })
+
+    }
+
+    fn encode(&self, data: &mut Value) {
+        data.set("type", "combat:send-attack");
+        self.header.encode(data);
+        self.auth.encode(data);
+        data.set("target", &self.target);
+        data.set("attack", &self.attack);
+    }
+}
