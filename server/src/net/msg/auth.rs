46a4c935cf4e7fb13e0b57198fad3dd82429f1d9
KokaKiwi <kokakiwi@kokakiwi.net>
2016-06-09 00:41:48 +0900

    Re-add session fetching methods to Authenticate message.

--- server/src/net/msg/auth.rs
+++ server/src/net/msg/auth.rs
@@ -1,13 +1,25 @@
-use super::{MessagePart, MessageHeader};
+use db::Database;
+use db::models::token::Session;
+use jwt;
 use net::msg::error::Result;
 use net::msg::utils::fields;
 use net::msg::utils::rmp::ValueExt;
 use rmp::Value;
+use super::{MessagePart, MessageHeader};
+
+const TOKEN_ALG: jwt::Algorithm = jwt::Algorithm::HS512;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Authenticate {
     pub header: MessageHeader,
     pub token: String,
+}
+
+impl Authenticate {
+    pub fn session(&self, secret: &str) -> ::std::result::Result<SessionData, jwt::errors::Error> {
+        let token = try!(jwt::decode(&self.token, secret.as_bytes(), TOKEN_ALG));
+        Ok(token.claims)
+    }
 }
 
 impl MessagePart for Authenticate {
@@ -22,5 +34,21 @@ impl MessagePart for Authenticate {
         data.set("type", "authenticate");
         self.header.encode(data);
         data.set("token", &self.token);
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, RustcDecodable, RustcEncodable)]
+pub struct SessionData {
+    pub session_id: u64,
+}
+
+impl SessionData {
+    pub fn db(&self, db: &Database) -> ::db::error::Result<Option<Session>> {
+        let mut session = try!(db.get_model::<Session>(self.session_id as i32));
+        if let Some(ref mut session) = session {
+            let conn = try!(db.get_connection());
+            try!(session.user.fetch(&conn));
+        }
+        Ok(session)
     }
 }
