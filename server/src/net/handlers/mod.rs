dc14eccefd769bf803e14b3f0bd1bac2b9d50466
Aude Lejuez <dusterherz@gmail.com>
2016-06-25 16:23:12 +0200

    Add handlers to the main handler with errors

--- server/src/net/handlers/mod.rs
+++ server/src/net/handlers/mod.rs
@@ -8,6 +8,9 @@ mod combat;
 pub fn handle(conn: &mut Connection, msg: Message) {
     match msg {
         Message::Authenticate(msg) => auth::handle(conn, msg),
+        Message::Combat::SendAttack(msg) => combat::send_attack::handle(conn, msg),
+        Message::Combat::Flee(msg) => combat::flee::handle(conn, msg),
+        Message::Combat::MonsterKo::Capture(msg) => combat::monster_ko_capture::handle(conn, msg),
         _ => {}
     }
 }
