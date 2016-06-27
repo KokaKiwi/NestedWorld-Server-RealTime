f010fcafba86b1ff6247cfb63a9ef2337ef3050b
Aude Lejuez <dusterherz@gmail.com>
2016-06-27 18:47:28 +0200

    Error in the sentence corrected

--- server/src/net/handlers/combat/send_attack.rs
+++ server/src/net/handlers/combat/send_attack.rs
@@ -7,7 +7,7 @@ pub fn handle(conn: &mut Connection, msg: SendAttack) {
     handle_with_result(conn, &msg.header, |conn| {
         match conn.session {
             Some(_) => { return ResultData::ok(None)},
-            None => { return ResultData::err("NotAuthenticated", "You are not authenticate on the server", None)}
+            None => { return ResultData::err("NotAuthenticated", "You are not authenticated on the server", None)}
         }
     });
 }
