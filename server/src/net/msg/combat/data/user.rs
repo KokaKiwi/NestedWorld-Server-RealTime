a97512e57ab0d2e03d4cbe42025d36a608ff88ec
KokaKiwi <kokakiwi@kokakiwi.net>
2016-07-05 22:10:05 +0200

    Update code corresponding to doc changes.

--- server/src/net/msg/combat/data/user.rs
+++ server/src/net/msg/combat/data/user.rs
@@ -1,4 +1,4 @@
-use net::msg::{MessagePart};
+use net::msg::MessagePart;
 use net::msg::error::Result;
 use net::msg::utils::fields;
 use net::msg::utils::rmp::ValueExt;
@@ -6,21 +6,17 @@ use rmp::Value;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct User {
-    pub id: u32,
-    pub name: String,
+    pub pseudo: String,
 }
-
 
 impl MessagePart for User {
     fn decode(data: &Value) -> Result<User> {
         Ok(User {
-            id: try!(fields::get(data, "id")),
-            name: try!(fields::get(data, "name")),
+            pseudo: try!(fields::get(data, "pseudo")),
         })
     }
 
     fn encode(&self, data: &mut Value) {
-        data.set("id", &self.name);
-        data.set("user", &self.id);
+        data.set("pseudo", &self.pseudo);
     }
 }
