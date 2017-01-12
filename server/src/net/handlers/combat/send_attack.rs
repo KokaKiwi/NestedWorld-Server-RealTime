c05c3aa4d7dba08af83c1d968eddf3e183c50f50
Aude Lejuez <dusterherz@gmail.com>
2017-01-12 02:35:55 +0100

    no more handle with result for attack

--- server/src/net/handlers/combat/send_attack.rs
+++ server/src/net/handlers/combat/send_attack.rs
@@ -1,25 +1,19 @@
 use net::conn::Connection;
 use net::msg::combat::SendAttack;
 use net::msg::result::ResultData;
-use net::handlers::helpers::result::handle_with_result;
+use net::msg::combat::Message;
+use net::handlers::helpers::result::send_result;
 
 pub fn handle(conn: &mut Connection, msg: SendAttack) {
-    handle_with_result(conn, &msg.header.clone(), |conn| {
-        use net::msg::combat::Message;
-
         let session = match conn.session() {
             Some(session) => session,
-            None => return ResultData::err("NotAuthenticated",
-                                           "You are not authenticated", None),
+            None => {send_result(conn, &msg.header, ResultData::err("NotAuthenticated", "You are not authenticated on the server", None)); return}
         };
         let user = session.user.get().expect("No user?");
 
         let mut combat = match conn.ctx.get_combat(msg.combat) {
             Some(combat) => combat,
-            None => return ResultData::err("InvalidCombat", "Invalid combat ID", None),
+            None => {send_result(conn, &msg.header, ResultData::err("InvalidCombat", "Invalid combat ID", None)); return}
         };
         combat.send(user.id as u32, &Message::SendAttack(msg));
-
-        ResultData::ok(None)
-    });
 }
